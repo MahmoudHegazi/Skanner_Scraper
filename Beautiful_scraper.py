1915609b108c8347ac215324034a3ebf7823b07b
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-11-06 15:07:10 +0100

    Create Beautiful_scraper.py

---
+++ Beautiful_scraper.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+from bs4 import BeautifulSoup
+from tablib import Dataset
+import numpy as np
+import excel
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, report, report_meta, Vote
+from flask import session as login_session
+import string
+import excel
+# IMPORTS FOR THIS STEP
+import httplib2
+import json
+from flask import make_response
+import requests
+import pandas as pd
+from tablib import Dataset
+import numpy as np
+import excel
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+# IMPORTS FOR THIS STEP
+import pprint
+import httplib2
+import json
+import sqlite3
+from flask import make_response
+import requests
+
+
+app = Flask(__name__)
+
+APPLICATION_NAME = "skaner"
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///skanner.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+
+# You need install :
+# pip install PyPDF2 - > Read and parse your content pdf
+# pip install requests - > request for get the pdf
+# pip install BeautifulSoup - > for parse the html and find all url hrf with ".pdf" final
+from PyPDF2 import PdfFileReader
+import requests
+import io
+from bs4 import BeautifulSoup
+
+@app.route('/')
+def skanner():
+    with open('index.html', 'r') as f:
+        contents = f.read()
+        soup = BeautifulSoup(contents, 'lxml')
+        projectData = []
+        #projectData.append(str(soup.h2.name))
+        #projectData.append(str(soup.h2.text))
+        #projectData.append(str(soup.li))
+        for child in soup.recursiveChildGenerator():
+
+            if child.name:
+                projectData.append(str(child.name))
+        return str(projectData)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'AS&S^1234Aoshsheo152h23h5j7ks9-1---3*-s,#k>s'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000, threaded=False)
